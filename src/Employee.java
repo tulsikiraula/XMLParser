dd2a64975e36708b6be464bfab0b91200f23a01e
tulsi kiraula <tukiraula@axway.com>
2017-07-19 15:09:59 +0530

    SAX Commit

---
+++ src/Employee.java
@@ -0,0 +1,45 @@
+
+public class Employee {
+	private int id;
+	private String name;
+	    public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender
+				+ ", age=" + age + ", role=" + role + "]";
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+		private String gender;
+	    private int age;
+	    private String role;
+	
+
+}
